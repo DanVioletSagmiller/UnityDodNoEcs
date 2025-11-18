7610fb39c6604218dd0507f518e4c17fe6f919d3
Dan Violet Sagmiller <DanVioletSagmiller@Outlook.com>
2025-11-18 14:21:59 +1100

    Demonstration of performance increase with second array

--- Assets/!Game/Scripts/Grid2InstancedRenderer.cs
+++ Assets/!Game/Scripts/Grid2InstancedRenderer.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class GridInstancedRenderer : MonoBehaviour
+public class Grid2InstancedRenderer : MonoBehaviour
 {
     public Mesh mesh;
     public Material material;
@@ -25,6 +25,7 @@ public class GridInstancedRenderer : MonoBehaviour
     
     
     Matrix4x4[][] _batches;
+    private float[][] _distances;
 
     void OnEnable()
     {
@@ -51,12 +52,13 @@ public class GridInstancedRenderer : MonoBehaviour
         var rotation = Quaternion.Euler(eulerRotation);
         Vector3 offset = Vector3.zero - new Vector3(GridSize.x * BatchSize.x * spacing, 0, GridSize.y * BatchSize.y * spacing) * .5f;
         _batches = new Matrix4x4[GridSize.x * GridSize.y][];
+        _distances = new float[GridSize.x * GridSize.y][];
         for (int gridX = 0; gridX < GridSize.x; gridX++)
         {
             for (int gridY = 0; gridY < GridSize.y; gridY++)
             {
                 var batch = new Matrix4x4[BatchSize.x * BatchSize.y];
-                
+                var distanceBatch = new float[BatchSize.x * BatchSize.y];
                 for (int batchX = 0; batchX < BatchSize.x; batchX++)
                 {
                     for (int batchY = 0; batchY < BatchSize.y; batchY++)
@@ -68,12 +70,15 @@ public class GridInstancedRenderer : MonoBehaviour
                                           (batchX + (gridX * BatchSize.x)) * spacing, 
                                           0f, 
                                           (batchY + (gridY * BatchSize.y) ) * spacing);
+                        
                         batch[batchIndex] = Matrix4x4.TRS(pos, rotation, scale);
+                        distanceBatch[batchIndex] = Vector3.Distance(Vector3.zero, pos) / WaveLength; 
                     }
                 }
                 
                 int gridIndex =  gridX * GridSize.y + gridY;
                 _batches[gridIndex] = batch;
+                _distances[gridIndex] = distanceBatch;
             }
         }
     }
@@ -87,12 +92,12 @@ public class GridInstancedRenderer : MonoBehaviour
         for (int gridIndex = 0; gridIndex < GridSize.x * GridSize.y; gridIndex++)
         {
             var batch = _batches[gridIndex];
-
+            var distances = _distances[gridIndex];
             for (int batchIndex = 0; batchIndex < batch.Length; batchIndex++)
             {
                 // Position in R3, in C0=X, C1=Y, C2=Z
                 var matrix = batch[batchIndex];
-                matrix.m13 = Amplitude * Mathf.Sin(WaveLength * (time + matrix.m03)) * .5f + Mathf.Sin(WaveLength * (time + matrix.m23)) * .5f;
+                matrix.m13 = Amplitude * Mathf.Sin(distances[batchIndex] * time) * .5f;
                 batch[batchIndex] = matrix;
             }
             
